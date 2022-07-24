c88a3131c7c30c263b162bd0f739ca6e7d98abd1
Ahmad <ahmdaf420@gmail.com>
2022-07-24 18:49:48 +0300

    Add project files.

---
+++ ConsoleApp85/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp85
+{
+    internal partial class Program
+    {
+        class Employee
+        {
+            private int id;
+            private string name;
+            private int age;
+            private double salary;
+            private double experince;
+
+            public Employee(){ }
+
+            public Employee(int id, string name, int age, double salary, double experince)
+            {
+                this.id = id;
+                this.name = name;
+                this.age = age;
+                this.salary = salary;
+                this.experince = experince;
+
+            }
+
+            public void employeeSayHello()
+            {
+                Console.WriteLine($"Hello my name is {this.name}.");
+            }
+
+        }
+    }
+}
